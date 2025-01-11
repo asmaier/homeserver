12279d61669e0b1c9847052c8979d5d4e7440ca3
asmaier <asmaier@web.de>
2025-01-11 23:40:24 +0100

    Using umbrelOS for installing apps for now.

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from pyinfra.operations import files, server
 
-for dir_name in ["watchtower", "portainer", "pihole", "filebrowser"]:
+for dir_name in ["umbrel"]:
     compose = files.put("apps/" + dir_name + "/docker-compose.yml", "docker/" + dir_name + "/docker-compose.yml")
-    if dir_name == "filebrowser":
-        files.put("apps/" + dir_name + "/filebrowser.json", "docker/" + dir_name + "/filebrowser.json")
-        files.put("apps/" + dir_name + "/filebrowser.db", "docker/" + dir_name + "/filebrowser.db")
     if compose.changed:
         server.shell("docker-compose down --remove-orphans && docker-compose up -d",
                      _shell_executable="zsh -l", _chdir="docker/" + dir_name)
--- stop.py
+++ stop.py
@@ -1,5 +1,5 @@
 from pyinfra.operations import server
 
-for dir_name in ["watchtower", "portainer", "pihole", "nextcloud", "filebrowser"]:
+for dir_name in ["umbrel"]:
     server.shell("docker-compose down --remove-orphans", _shell_executable="zsh -l", _chdir="docker/" + dir_name)
 
